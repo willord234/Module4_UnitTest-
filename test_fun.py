55980dfd1afb6fec5ab473fd0dec26c859af49f8
Willord Ploteña <98720001+willord234@users.noreply.github.com>
2022-02-23 09:40:32 +0800

    Add files via upload

---
+++ test_fun.py
@@ -0,0 +1,15 @@
+import unittest
+
+def fun(x):
+
+    return x+1
+
+
+class MyTest(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(fun(5),6)
+
+if __name__== '__main__':
+
+    unittest.main()
